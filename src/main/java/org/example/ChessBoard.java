de7fdb0923e76dced4bb2a0117faee271481863f
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-10-01 16:12:47 +0200

    added tests and logic for checking if a piece can stand on a particular box

--- src/main/java/org/example/ChessBoard.java
+++ src/main/java/org/example/ChessBoard.java
@@ -7,60 +7,69 @@ public class ChessBoard {
     public ChessBoard() {
         board = new Piece[8][8];
         for (int i = 0; i < 8; i++) {
-            Piece pawn = new Piece("Pawn " + i, "white");
-            board[1][i] = pawn;
+            Piece pawn = new Piece("Pawn", "white");
+            placePiece(pawn, 1, i);
         }
         for (int i = 0; i < 8; i++) {
-            Piece pawn = new Piece("Pawn " + i, "black");
-            board[6][i] = pawn;
+            Piece pawn = new Piece("Pawn", "black");
+            placePiece(pawn, 6, i);
         }
         for (int i = 0; i < 2; i++) {
-            Piece knight = new Piece("Horse " + i, "white");
-
-        }
-        for (int i = 0; i < 2; i++) {
-            Piece knight = new Piece("Horse " + i, "black");
-        }
-        for (int i = 0; i < 2; i++) {
-            Piece rook = new Piece("Rook " + i, "white");
+            Piece knight = new Piece("Knight", "white");
             if(i == 0){
-                board[0][0] = rook;
+                placePiece(knight, 0, 1);
             } else {
-                board[0][7] = rook;
+                placePiece(knight, 0, 6);
             }
         }
         for (int i = 0; i < 2; i++) {
-            Piece rook = new Piece("Rook " + i, "black");
+            Piece knight = new Piece("Knight", "black");
             if(i == 0){
-                board[7][0] = rook;
+                placePiece(knight, 7, 1);
             } else {
-                board[7][7] = rook;
+                placePiece(knight, 7, 6);
             }
         }
         for (int i = 0; i < 2; i++) {
-            Piece bishop = new Piece("Bishop " + i, "white");
+            Piece rook = new Piece("Rook", "white");
             if(i == 0){
-                board[0][2] = bishop;
+                placePiece(rook, 0, 0);
             } else {
-                board[0][5] = bishop;
+                placePiece(rook, 0, 7);
             }
         }
         for (int i = 0; i < 2; i++) {
-            Piece bishop = new Piece("Bishop " + i, "black");
+            Piece rook = new Piece("Rook", "black");
             if(i == 0){
-                board[7][2] = bishop;
+                placePiece(rook, 7, 0);
             } else {
-                board[7][5] = bishop;
+                placePiece(rook, 7, 7);
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            Piece bishop = new Piece("Bishop", "white");
+            if(i == 0){
+                placePiece(bishop, 0, 2);
+            } else {
+                placePiece(bishop, 0, 5);
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            Piece bishop = new Piece("Bishop", "black");
+            if(i == 0){
+                placePiece(bishop, 7, 2);
+            } else {
+                placePiece(bishop, 7, 5);
             }
         }
         Piece king = new Piece("King", "white");
-        board[0][4] = king;
+        placePiece(king, 0, 3);
         Piece queen = new Piece("Queen", "white");
-        board[0][3] = queen;
-        Piece king2 = new Piece("King 2", "black");
-        board[7][4] = king2;
-        Piece queen2 = new Piece("Queen 2", "black");
-        board[7][3] = queen2;
+        placePiece(queen, 0, 4);
+        Piece king2 = new Piece("King", "black");
+        placePiece(king2, 7, 3);
+        Piece queen2 = new Piece("Queen", "black");
+        placePiece(queen2, 7, 4);
     }
 
     public void placePiece(Piece piece, int row, int col) {
@@ -69,11 +78,47 @@ public class ChessBoard {
         }
     }
 
-    // Hämta en Piece från en specifik koordinat
     public Piece getPiece(int row, int col) {
         if (row >= 0 && row < 8 && col >= 0 && col < 8) {
             return board[row][col];
         }
         return null;
+    }
+
+    public boolean canMove(int fromRow, int fromColumn, int toRow, int toColumn){
+        if(getPiece(fromRow, fromColumn)!=null ){
+            if(getPiece(toRow, toColumn)==null){
+                return true;
+            } else if(getPiece(fromRow, fromColumn).getColor().equals(getPiece(toRow, toColumn).getColor())){
+                return false;
+            } else {
+                return true;
+            }
+
+        } return false;
+    }
+//            switch(getPiece(fromRow, fromColumn).getName()){
+//                case "Pawn":
+//
+//                case "Knight":
+//                    break;
+//                case "Rook":
+//                    break;
+//                case "Bishop":
+//                    break;
+//                case "King":
+//                    break;
+//                case "Queen":
+//                    break;
+//                default:
+//                    return false;
+//            }
+//            return true;
+//        }
+//        return false;
+
+
+    public void resetBoard(){
+
     }
 }
--- src/test/java/org/example/ChessBoardTest.java
+++ src/test/java/org/example/ChessBoardTest.java
@@ -2,26 +2,53 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 
 class ChessBoardTest {
 
-    @Test
-    void testChessBoard() {
+
+
         ChessBoard board = new ChessBoard();
 
-        Piece pawn = new Piece("Pawn", "white");
-        Piece knight = new Piece("Knight", "white");
-        Piece bishop = new Piece("Bishop", "white");
-        Piece rook = new Piece("Rook", "white");
-        Piece queen = new Piece("Queen", "white");
-        Piece king = new Piece("King", "white");
-        Piece queen2 = new Piece("Queen", "black");
-        Piece king2 = new Piece("King", "black");
-        Piece rook2 = new Piece("Rook", "black");
-        Piece pawn2 = new Piece("Pawn", "black");
-        Piece knight2 = new Piece("Knight", "black");
-        Piece bishop2 = new Piece("Bishop", "black");
+        @Test
+        void testSetPiece(){
+            Piece testKnight = new Piece("Knight", "black");
+            board.placePiece(testKnight, 2, 2);
+            assertEquals(testKnight, board.getPiece(2, 2));
+        }
 
-        board.placePiece();
-    }
+        @Test
+        void testGetPiece() {
+        Piece a = board.getPiece(3,3);
+        Piece b = board.getPiece(-1, -1);
+        Piece c = board.getPiece(0, 2);
+        Piece d = board.getPiece(8, 8);
+
+
+        //then
+        assertNull(a);
+        assertNull(b);
+        assertNull(d);
+        assertEquals( c , board.getPiece(0, 2));
+        assertEquals("Knight 0", board.getPiece(0, 1).getName());
+        assertEquals( "white" , board.getPiece(0, 1).getColor());
+        assertNull(board.getPiece(3, 3));
+        assertNull(board.getPiece(5, 5));
+        }
+
+        @Test
+        void testMove(){
+            boolean move1 = board.canMove(0,0, 3,3);
+            boolean move2 = board.canMove(1,0, 1,2);
+            boolean move3 = board.canMove(3,3, 5,5);
+            boolean move4 = board.canMove(-1,-1, 1,2);
+            boolean move5 = board.canMove(6,0, 1,2);
+                assertTrue(move1);
+                assertFalse(move2);
+                assertFalse(move3);
+                assertFalse(move4);
+                assertTrue(move5);
+        }
+
 }
